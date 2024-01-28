3041e1b12d39b71d840ccc68d76a7973bd3ca6d1
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-01-28 20:02:30 +0100

    feat: setup button component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+  import Button from "./components/Button.svelte";
 </script>
 
 <main>
-  <h1>pathfinder</h1>
+  <h1>§pathfinder</h1>
+  <Button fullWidth>§Play</Button>
 </main>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: var(--spacing-md);
+    height: 100vh;
+    widows: 100vw;
+  }
 </style>
---
+++ src/components/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let fullWidth = false;
+</script>
+
+<button class:full-width={fullWidth}><slot /></button>
+
+<style lang="scss">
+  button {
+    border-radius: 0;
+    border: 0.125rem var(--primary-color) solid;
+    outline: none;
+    background-color: var(--background-color);
+    cursor: pointer;
+    padding: var(--spacing-xs) var(--spacing-lg);
+
+    &:hover {
+      background-color: var(--primary-color);
+      color: var(--light-text-color);
+    }
+
+    &.full-width {
+      width: 50vw;
+    }
+  }
+</style>
