b5b0d61e5abb1f06ace092b2e92833a8f2459d8f
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-02-14 13:16:32 +0100

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
+
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 import * as path from "path";
@@ -25,10 +27,7 @@ export default defineConfig({
     }),
   ],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -36,15 +35,15 @@ export default defineConfig({
   resolve: {
     alias: paths,
   },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
   build: {
-    // Tauri supports es2021
     target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  test: {
+    includeSource: ["src/**/*.ts"],
+    exclude: ["node_modules"],
+    clearMocks: true,
   },
 });
