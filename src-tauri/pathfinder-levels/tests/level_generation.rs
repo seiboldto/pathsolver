26cc3b86dd5d0c715b06da9904cd831a61fa666f
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-02-14 13:05:31 +0100

    chore: remove wasm

--- src-tauri/pathfinder-levels/tests/level_generation.rs
+++
@@ -1,7 +0,0 @@
-use pathfinder_levels::{generate_level, Difficulty};
-
-#[test]
-fn it_generates_levels() {
-    let difficulty = Difficulty::new(3, Vec::new());
-    generate_level("seed", difficulty);
-}
--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,3 @@ const app = new App({
 });
 
 export default app;
-
-/*
-import init, { greet } from "pathfinder-levels";
-// Don't worry if vscode told you can't find my-crate
-// It's because you're using a local crate
-// after yarn dev, wasm-pack plugin will install my-crate for you
-
-init().then(() => {
-  console.log("init wasm-pack");
-  greet("from vite!");
-});
-*/
