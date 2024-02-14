f59b0d6b719463562b5b0ab99fe7e3c0f1f4873b
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-02-14 21:02:05 +0100

    feat: generate random operation

--- src/levels/difficulty.test.ts
+++ src/levels/difficulty.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest";
+import prand from "pure-rand";
 
 import { Difficulty } from "./difficulty";
 import { type OperationKind } from "./operation";
@@ -7,34 +8,67 @@ describe("Difficulty", () => {
   it.each<{
     difficulty: "normal" | "hard" | "extreme";
     boardSize: number;
-    possibleOperations: OperationKind[];
+    operationDistribution: Record<OperationKind, number>;
   }>([
     {
       difficulty: "normal",
       boardSize: 3,
-      possibleOperations: ["addition", "subtraction"],
+      operationDistribution: {
+        addition: 70,
+        subtraction: 30,
+        multiplication: 0,
+        division: 0,
+      },
     },
     {
       difficulty: "hard",
       boardSize: 3,
-      possibleOperations: ["addition", "subtraction", "multiplication"],
+      operationDistribution: {
+        addition: 65,
+        subtraction: 25,
+        multiplication: 10,
+        division: 0,
+      },
     },
     {
       difficulty: "extreme",
       boardSize: 4,
-      possibleOperations: [
-        "addition",
-        "subtraction",
-        "multiplication",
-        "division",
-      ],
+      operationDistribution: {
+        addition: 65,
+        subtraction: 20,
+        multiplication: 10,
+        division: 5,
+      },
     },
   ])(
     "returns the correct $difficulty preset",
-    ({ difficulty, boardSize, possibleOperations }) => {
+    ({ difficulty, boardSize, operationDistribution }) => {
       const d = Difficulty[difficulty]();
       expect(d.boardSize).toEqual(boardSize);
-      expect(d.possibleOperations).toEqual(possibleOperations);
+      expect(d.operationDistribution).toEqual(operationDistribution);
     },
   );
+
+  it("generates random operations according to specified weights", () => {
+    const N = 1_000;
+    const DELTA = N / 10 / 2;
+
+    const operations: Partial<Record<OperationKind, number>> = {};
+
+    const difficulty = Difficulty.extreme();
+    const rng = prand.xorshift128plus(0);
+
+    for (let i = 0; i < N; i++) {
+      const op = difficulty.getRandomOperation(rng);
+      const cur = operations[op] ?? 0;
+      operations[op] = cur + 1;
+    }
+
+    for (const k of Object.keys(difficulty.operationDistribution)) {
+      const key = k as OperationKind;
+      const expected = difficulty.operationDistribution[key] * (N / 100);
+      const received = operations[key] ?? 0;
+      expect(Math.abs(received - expected)).toBeLessThan(DELTA);
+    }
+  });
 });
--- src/levels/difficulty.ts
+++ src/levels/difficulty.ts
@@ -1,39 +1,101 @@
-import type { OperationKind } from "./operation";
+import prand, { type RandomGenerator } from "pure-rand";
+
+import { type OperationKind } from "./operation";
 
 /** Defines the difficulty of a level.
  *
  * To create a custom difficulty, use the constructor.
  * Otherwise, you can use a predefined preset.
  *
- * | ID      | Board Size | Possible Operations |
- * |---------|------------|---------------------|
- * | Normal  | 3          | + -                 |
- * | Hard    | 3          | + - *               |
- * | Extreme | 4          | + - * /             |
+ * | ID      | Board Size | Operation Distribution         |
+ * |---------|------------|--------------------------------|
+ * | Normal  | 3          | + (70%) - (30%)                |
+ * | Hard    | 3          | + (65%) - (25%) * (10%)        |
+ * | Extreme | 4          | + (65%) - (20%) * (10%) / (5%) |
  */
 export class Difficulty {
   public boardSize: number;
-  public possibleOperations: OperationKind[];
+  public operationDistribution: Record<OperationKind, number>;
 
-  constructor(boardSize: number, possibleOperations: OperationKind[]) {
+  constructor(
+    boardSize: number,
+    operationDistribution: Partial<Record<OperationKind, number>>,
+  ) {
     this.boardSize = boardSize;
-    this.possibleOperations = possibleOperations;
+
+    const weightsSum = Object.values(operationDistribution).reduce(
+      (a, b) => a + b,
+      0,
+    );
+    const multiplier = 100 / weightsSum;
+    const normalizedDistribution: Record<OperationKind, number> = {
+      addition: 0,
+      subtraction: 0,
+      multiplication: 0,
+      division: 0,
+    };
+    for (const [k, value] of Object.entries(operationDistribution)) {
+      const key = k as OperationKind;
+      normalizedDistribution[key] = value * multiplier;
+    }
+
+    this.operationDistribution = normalizedDistribution;
   }
 
-  static normal() {
-    return new Difficulty(3, ["addition", "subtraction"]);
+  /**
+   * Generates a random operation based on the operation distribution.
+   * @param rng - A random generator instance
+   * @returns A random operation
+   */
+  public getRandomOperation(rng: RandomGenerator): OperationKind {
+    const operations = Object.keys(
+      this.operationDistribution,
+    ) as OperationKind[];
+    const weights = Object.values(this.operationDistribution);
+
+    const summedWeights = [weights[0]];
+    for (let i = 1; i < weights.length; i++)
+      summedWeights[i] = summedWeights[i - 1] + weights[i];
+
+    const random = prand.unsafeUniformIntDistribution(
+      0,
+      summedWeights[summedWeights.length - 1],
+      rng,
+    );
+
+    let i = 0;
+    while (summedWeights[i] < random) i++;
+
+    return operations[i];
   }
 
-  static hard() {
-    return new Difficulty(3, ["addition", "subtraction", "multiplication"]);
+  /**
+   * Preset normal difficulty.
+   */
+  static normal(): Difficulty {
+    return new Difficulty(3, { addition: 70, subtraction: 30 });
   }
 
-  static extreme() {
-    return new Difficulty(4, [
-      "addition",
-      "subtraction",
-      "multiplication",
-      "division",
-    ]);
+  /**
+   * Preset hard difficulty.
+   */
+  static hard(): Difficulty {
+    return new Difficulty(3, {
+      addition: 65,
+      subtraction: 25,
+      multiplication: 10,
+    });
+  }
+
+  /**
+   * Preset extreme difficulty.
+   */
+  static extreme(): Difficulty {
+    return new Difficulty(4, {
+      addition: 65,
+      subtraction: 20,
+      multiplication: 10,
+      division: 5,
+    });
   }
 }
