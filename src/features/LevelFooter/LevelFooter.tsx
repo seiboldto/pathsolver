eb0454961a0b4f8dc291a0628dfbb9ec8abfd972
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-03-31 18:40:25 +0200

    feat: animate selected value

--- src/features/LevelFooter/LevelFooter.tsx
+++ src/features/LevelFooter/LevelFooter.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { useLevelStore } from "~src/stores";
 
 import classes from "./LevelFooter.module.css";
@@ -5,5 +7,15 @@ import classes from "./LevelFooter.module.css";
 export function LevelFooter(): JSX.Element {
   const selectedValue = useLevelStore.use.selectedValue();
 
-  return <div className={classes.selectedValue}>{selectedValue}</div>;
+  return (
+    <div className={classes.selectedValue}>
+      <motion.span
+        key={selectedValue}
+        initial={{ scale: 1.3 }}
+        animate={{ scale: 1 }}
+      >
+        {selectedValue}
+      </motion.span>
+    </div>
+  );
 }
