a8f63129efe0b1f0ae00db66247705b433c2bf34
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-06-01 12:25:43 +0200

    chore: remove unneccessary comments

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { Button } from "./Button/Button";
-export { Checkbox } from "./Checkbox/Checkbox"; // X
-export { Divider } from "./Divider/Divider"; // X
-export { Group } from "./Group/Group"; // X
-export { Screen } from "./Screen/Screen"; // X
-export { Select } from "./Select/Select"; // X
-export { Title } from "./Title/Title"; // X
-export { ToggleInput } from "./ToggleInput/ToggleInput"; // X
+export { Checkbox } from "./Checkbox/Checkbox";
+export { Divider } from "./Divider/Divider";
+export { Group } from "./Group/Group";
+export { Screen } from "./Screen/Screen";
+export { Select } from "./Select/Select";
+export { Title } from "./Title/Title";
+export { ToggleInput } from "./ToggleInput/ToggleInput";
