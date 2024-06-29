7a4013cd34f542318c548dc138929f820518d9c1
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-06-29 22:49:02 +0200

    feat: make objectives accessible

--- src/features/Objectives/Objectives.tsx
+++ src/features/Objectives/Objectives.tsx
@@ -1,18 +1,34 @@
 import { IconCheck } from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
 
 import { useActiveLevel } from "~src/hooks";
 
 import classes from "./Objectives.module.css";
 
 export function Objectives(): JSX.Element {
+  const { t } = useTranslation();
   const { objectives, getObjectiveState } = useActiveLevel();
 
   return (
-    <div className={classes.objectives}>
+    <div
+      className={classes.objectives}
+      role="group"
+      aria-label={t("game.objectives.title")}
+    >
       {objectives.map((o) => {
         const state = getObjectiveState(o);
         return (
-          <div key={o.id} className={classes.objective} data-state={state}>
+          <div
+            key={o.id}
+            className={classes.objective}
+            data-state={state}
+            aria-current={state === "active" ? "step" : "false"}
+            aria-label={t("game.objectives.index", {
+              count: o.index + 1,
+              ordinal: true,
+            })}
+            aria-description={t(`game.objectives.${state}`)}
+          >
             {state === "completed" ? <IconCheck /> : o.value}
           </div>
         );
