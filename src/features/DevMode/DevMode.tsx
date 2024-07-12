7b5afacc4a7a1e55d8650fb041760af02ef8d0b9
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-07-12 10:38:06 +0200

    feat: dev mode hint

--- src/features/DevMode/DevMode.tsx
+++ src/features/DevMode/DevMode.tsx
@@ -1,25 +1,60 @@
-import { IconCode } from "@tabler/icons-react";
+import { IconBulb, IconCode } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
+import { useLocation } from "wouter";
 
 import { Button, Tooltip } from "~src/components";
-import { useUiStore } from "~src/stores";
+import { useLevelStore, useUiStore } from "~src/stores";
 
 import classes from "./DevMode.module.css";
 
+const print = (...message: string[]) => {
+  console.log(message.join("\n"));
+};
+
 export function DevMode() {
   const { t } = useTranslation();
+  const [location] = useLocation();
 
   const isDeveloperMode = useUiStore.use.isDeveloperMode();
   const { toggleDeveloperMode } = useUiStore.use.actions();
 
+  const activeLevelState = useLevelStore.use.activeLevelState();
+
+  const handlePrintObjectives = () => {
+    if (!activeLevelState) return;
+
+    const { objectives } = activeLevelState;
+
+    print(
+      ...objectives.flatMap((o) => [
+        "Objective " + o.index,
+        o.path.map((i) => i.row + ", " + i.column).join(" -> "),
+      ])
+    );
+  };
+
+  const disableLevelCheats = location !== "/level";
+
   return (
     <div className={classes.devMode}>
-      <Tooltip label={t("developer-mode.title")}>
+      <Tooltip label={t("dev-mode.title")}>
         <Button square onClick={toggleDeveloperMode}>
           <IconCode />
         </Button>
       </Tooltip>
-      {isDeveloperMode && <>buttons</>}
+      {isDeveloperMode && (
+        <>
+          <Tooltip label={t("dev-mode.print-objectives")}>
+            <Button
+              square
+              onClick={handlePrintObjectives}
+              disabled={disableLevelCheats}
+            >
+              <IconBulb />
+            </Button>
+          </Tooltip>
+        </>
+      )}
     </div>
   );
 }
