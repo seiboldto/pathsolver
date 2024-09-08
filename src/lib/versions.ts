8731cfa82b69d52b892e9164fc7ee84fccd4955a
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-09-08 15:36:26 +0200

    release: 1.0.0

--- src/lib/versions.ts
+++ src/lib/versions.ts
@@ -1,8 +1,8 @@
 export const VERSIONS = {
   STORAGE: {
-    PERSISTED_LEVEL: 0,
-    SETTINGS: 0,
-    STATISTICS: 0,
+    PERSISTED_LEVEL: 1,
+    SETTINGS: 1,
+    STATISTICS: 1,
   },
   GENERATOR: 1,
   APP_VERSION: __APP_VERSION__,
