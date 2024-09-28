1e4dcbec7a88c45e041876369d8e4768757456df
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-09-28 17:39:59 +0200

    ci: create html report

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "list",
+  reporter: [["list"], ["html", { open: "never" }]],
   use: {
     baseURL: "http://127.0.0.1:5173",
     trace: "on-first-retry",
